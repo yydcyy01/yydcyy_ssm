7b6bfc77eb1ddca27860f336a95d6c16fedc8fca
yuyang <447395536@qq.com>
2019-10-15 16:53:34 +0800

    完善 SpringSecurity 权限管理

---
+++ yydcyy_ssm_dao/src/main/java/com/yydcyy/ssm/dao/IRoleDao.java
@@ -0,0 +1,28 @@
+package com.yydcyy.ssm.dao;
+
+import com.yydcyy.ssm.domain.Role;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * @author YYDCYY
+ * @create 2019-10-15
+ */
+public interface IRoleDao {
+    //根据用户id查询出所有对应的角色
+    @Select("select * from role where id in (select roleId from users_role where userId=#{userId})")
+    @Results({
+            @Result(id = true, property = "id", column = "id"),
+            @Result(property = "roleName", column = "roleName"),
+            @Result(property = "roleDesc", column = "roleDesc"),
+            @Result(property = "permissions",column = "id",javaType = java.util.List.class,many = @Many(select = "com.itheima.ssm.dao.IPermissionDao.findPermissionByRoleId"))
+    })
+    public List<Role> findRoleByUserId(String userId) throws Exception;
+
+    @Select("select * from role")
+    List<Role> findAll() throws Exception;
+
+    @Insert("insert into role(roleName,roleDesc) values(#{roleName},#{roleDesc})")
+    void save(Role role);
+}
